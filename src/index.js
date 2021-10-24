4ed70fe318b3bf621f6a7b324afd50fd965529fb
Aleksandr <legionerab3000@yandex.ru>
2021-10-24 13:43:25 +0300

    Добавляет config и параметр rootUrlPath

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { APP_CONFIG } from './app/config';
 import { App } from './App';
 import { store } from './app/store';
 import './index.css';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={APP_CONFIG.rootUrlPath}>
             <Provider store={store}>
                 <App />
             </Provider>
